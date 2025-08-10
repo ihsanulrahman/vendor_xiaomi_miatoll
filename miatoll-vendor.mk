a224b31b902546fa5784cc3d8225312c42249e47
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-08-10 10:53:09 +0000

    miatoll: wfd configs stuff [temp]
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- miatoll-vendor.mk
+++ miatoll-vendor.mk
@@ -906,6 +906,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diaghal@1.0 \
     vendor.qti.hardware.fm@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
+    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
     vendor.qti.imsrtpservice@3.0 \
     CACertService \
     CneApp \
@@ -984,6 +985,7 @@ PRODUCT_PACKAGES += \
     wfdservice64
 
 PRODUCT_PACKAGES += \
+    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
     vendor_lib_libq3dtools_adreno_so \
